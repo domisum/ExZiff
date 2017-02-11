2520121e5803f8bfc9295e6c99546c25044d7acf
Dominik <dominik.weissenseel@gmx.de>
2017-02-11 12:41:01 +0100

    Added class to recenter shapes

---
+++ src/main/java/de/domisum/exziff/shape/transformer/RasterShapeTransformerRecenter.java
@@ -0,0 +1,54 @@
+package de.domisum.exziff.shape.transformer;
+
+import de.domisum.exziff.shape.RasterShape;
+
+public class RasterShapeTransformerRecenter extends RasterShapeTransformer
+{
+
+	// -------
+	// INITIALIZATION
+	// -------
+	public RasterShapeTransformerRecenter(RasterShape input)
+	{
+		super(input);
+	}
+
+
+	// -------
+	// TRANSFORMATION
+	// -------
+	@SuppressWarnings("ConstantConditions")
+	@Override
+	public void transform()
+	{
+		int minX = Integer.MAX_VALUE;
+		int maxX = Integer.MIN_VALUE;
+		int minY = Integer.MAX_VALUE;
+		int maxY = Integer.MIN_VALUE;
+
+		for(int y = 0; y < this.input.getHeight(); y++)
+			for(int x = 0; x < this.input.getWidth(); x++)
+				if(this.input.get(x, y))
+				{
+					if(x < minX)
+						minX = x;
+					if(x > maxX)
+						maxX = x;
+					if(y < minY)
+						minY = y;
+					if(y > maxY)
+						maxY = y;
+				}
+
+		int centerX = (maxX-minX)/2;
+		int centerY = (maxY-minY)/2;
+
+		int centerXOffset = centerX-(this.input.getWidth()/2);
+		int centerYOffset = centerY-(this.input.getHeight()/2);
+
+		RasterShapeTransformerShift shifter = new RasterShapeTransformerShift(this.input, -centerXOffset, -centerYOffset);
+		shifter.transform();
+		this.output = shifter.getOutput();
+	}
+
+}
