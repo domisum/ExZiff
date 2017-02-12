0c93f3d017b0e714a41adbcae63b09bc2693702d
Dominik <dominik.weissenseel@gmx.de>
2017-02-12 19:39:41 +0100

    Added comments and removed unneeded remove from FloodFill

--- src/main/java/de/domisum/exziff/shape/transformation/RasterShapeFloodFill.java
+++ src/main/java/de/domisum/exziff/shape/transformation/RasterShapeFloodFill.java
@@ -18,14 +18,16 @@ public class RasterShapeFloodFill extends RasterShapeTransformation
 	{
 		boolean[][] pixels = new boolean[input.getHeight()][input.getWidth()];
 
+		// these pixels have yet to be processed
 		Queue<PixelCoordinates> unvisitedPixels = new LinkedList<>();
+		// the pixels marked with true should not be added to the unvisitedPixels queue again
 		boolean[][] blockedPixels = new boolean[input.getHeight()][input.getWidth()];
 
 		// start floodfill from the outer edge
 		for(int y = 0; y < input.getHeight(); y++)
 			for(int x = 0; x < input.getWidth(); x++)
-				if(x == 0 || y == 0 || x == input.getWidth()-1 || y == input.getHeight()-1)
-					if(!input.get(x, y))
+				if(x == 0 || y == 0 || x == input.getWidth()-1 || y == input.getHeight()-1) // only the pixels on the outer edge
+					if(!input.get(x, y)) // and only if they are not blocked themselves
 					{
 						unvisitedPixels.add(new PixelCoordinates(x, y));
 						blockedPixels[y][x] = true;
@@ -34,8 +36,6 @@ public class RasterShapeFloodFill extends RasterShapeTransformation
 		while(unvisitedPixels.size() > 0)
 		{
 			PixelCoordinates coordinates = unvisitedPixels.poll();
-			unvisitedPixels.remove(coordinates);
-
 			pixels[coordinates.y][coordinates.x] = true;
 
 			tryAddNeighborPixel(input, coordinates, unvisitedPixels, blockedPixels, 1, 0);
