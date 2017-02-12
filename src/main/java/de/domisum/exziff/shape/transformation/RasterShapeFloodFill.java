b4849850d088aeb7337761a9a3e36278a9dbac6e
Dominik <dominik.weissenseel@gmx.de>
2017-02-12 19:14:33 +0100

    Improved floodfill speed even more

--- src/main/java/de/domisum/exziff/shape/transformation/RasterShapeFloodFill.java
+++ src/main/java/de/domisum/exziff/shape/transformation/RasterShapeFloodFill.java
@@ -3,8 +3,9 @@ package de.domisum.exziff.shape.transformation;
 import de.domisum.exziff.shape.PixelCoordinates;
 import de.domisum.exziff.shape.RasterShape;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RasterShapeFloodFill extends RasterShapeTransformation
 {
@@ -17,49 +18,52 @@ public class RasterShapeFloodFill extends RasterShapeTransformation
 	{
 		boolean[][] pixels = new boolean[input.getHeight()][input.getWidth()];
 
-		Set<PixelCoordinates> unvisitedPixels = new HashSet<>();
-		Set<PixelCoordinates> visitedPixels = new HashSet<>();
+		Queue<PixelCoordinates> unvisitedPixels = new LinkedList<>();
+		boolean[][] blockedPixels = new boolean[input.getHeight()][input.getWidth()];
 
 		// start floodfill from the outer edge
 		for(int y = 0; y < input.getHeight(); y++)
 			for(int x = 0; x < input.getWidth(); x++)
 				if(x == 0 || y == 0 || x == input.getWidth()-1 || y == input.getHeight()-1)
 					if(!input.get(x, y))
+					{
 						unvisitedPixels.add(new PixelCoordinates(x, y));
+						blockedPixels[y][x] = true;
+					}
 
 		while(unvisitedPixels.size() > 0)
 		{
 			PixelCoordinates coordinates = unvisitedPixels.iterator().next();
 			unvisitedPixels.remove(coordinates);
-			visitedPixels.add(coordinates);
 
 			pixels[coordinates.y][coordinates.x] = true;
 
-			tryAddNeighborPixel(input, coordinates, unvisitedPixels, visitedPixels, 1, 0);
-			tryAddNeighborPixel(input, coordinates, unvisitedPixels, visitedPixels, -1, 0);
-			tryAddNeighborPixel(input, coordinates, unvisitedPixels, visitedPixels, 0, 1);
-			tryAddNeighborPixel(input, coordinates, unvisitedPixels, visitedPixels, 0, -1);
+			tryAddNeighborPixel(input, coordinates, unvisitedPixels, blockedPixels, 1, 0);
+			tryAddNeighborPixel(input, coordinates, unvisitedPixels, blockedPixels, -1, 0);
+			tryAddNeighborPixel(input, coordinates, unvisitedPixels, blockedPixels, 0, 1);
+			tryAddNeighborPixel(input, coordinates, unvisitedPixels, blockedPixels, 0, -1);
 		}
 
 		return new RasterShape(pixels);
 	}
 
-	private void tryAddNeighborPixel(RasterShape input, PixelCoordinates base, Set<PixelCoordinates> unvisitedPixels,
-			Set<PixelCoordinates> visitedPixels, int dX, int dY)
+	private void tryAddNeighborPixel(RasterShape input, PixelCoordinates base, Collection<PixelCoordinates> unvisitedPixels,
+			boolean[][] blockedPixels, int dX, int dY)
 	{
-		PixelCoordinates newCoordinates = new PixelCoordinates(base.x+dX, base.y+dY);
+		int nX = base.x+dX;
+		int nY = base.y+dY;
 
-		if(newCoordinates.x < 0 || newCoordinates.y < 0 || newCoordinates.x >= input.getWidth() || newCoordinates.y >= input
-				.getHeight())
+		if(nX < 0 || nY < 0 || nX >= input.getWidth() || nY >= input.getHeight())
 			return;
 
-		if(input.get(newCoordinates.x, newCoordinates.y))
+		if(blockedPixels[nY][nX])
+			return;
+		blockedPixels[nY][nX] = true;
+
+		if(input.get(nX, nY))
 			return;
 
-		if(visitedPixels.contains(newCoordinates))
-			return;
-
-		unvisitedPixels.add(newCoordinates);
+		unvisitedPixels.add(new PixelCoordinates(base.x+dX, base.y+dY));
 	}
 
 }
