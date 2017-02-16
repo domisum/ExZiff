1a5ee71db5ec94825f4187b03592ab16b28f03a2
Dominik <dominik.weissenseel@gmx.de>
2017-02-16 20:17:51 +0100

    Fixed shape smooth module

--- src/main/java/de/domisum/exziff/shape/transformation/RasterShapeSmooth.java
+++ src/main/java/de/domisum/exziff/shape/transformation/RasterShapeSmooth.java
@@ -9,12 +9,18 @@ public class RasterShapeSmooth extends RasterShapeTransformation
 	private final int radius;
 	private final double removeThreshold;
 	private final double addThreshold;
+	private final int iterations;
 
 
 	// -------
 	// INIT
 	// -------
 	public RasterShapeSmooth(int radius, double removeThreshold, double addThreshold)
+	{
+		this(radius, removeThreshold, addThreshold, 1);
+	}
+
+	public RasterShapeSmooth(int radius, double removeThreshold, double addThreshold, int iterations)
 	{
 		if(radius <= 0)
 			throw new IllegalArgumentException("The radius has to be at least 1");
@@ -24,9 +30,17 @@ public class RasterShapeSmooth extends RasterShapeTransformation
 		if(addThreshold < 0 || addThreshold > 1)
 			throw new IllegalArgumentException("The addThreshold has to be between 0 and 1");
 
+		if(removeThreshold > addThreshold)
+			throw new IllegalArgumentException("The removeThreshold has to be smaller than the addThreshold");
+
+		if(iterations <= 0)
+			throw new IllegalArgumentException("The number of iterations has to be greater than 0");
+
+
 		this.radius = radius;
 		this.removeThreshold = removeThreshold;
 		this.addThreshold = addThreshold;
+		this.iterations = iterations;
 	}
 
 
@@ -36,6 +50,15 @@ public class RasterShapeSmooth extends RasterShapeTransformation
 	@Override
 	public RasterShape transform(RasterShape input)
 	{
+		RasterShape deformedShape = input;
+		for(int i = 0; i < this.iterations; i++)
+			deformedShape = smooth(deformedShape);
+
+		return deformedShape;
+	}
+
+	private RasterShape smooth(RasterShape input)
+	{
 		boolean[][] pixels = new boolean[input.getHeight()][input.getWidth()];
 
 		int diameter = (this.radius+1+this.radius);
@@ -44,19 +67,20 @@ public class RasterShapeSmooth extends RasterShapeTransformation
 			for(int x = 0; x < input.getWidth(); x++)
 			{
 				double sum = 0;
-				for(int rY = -this.radius; rY <= this.radius; rY++)
-					for(int rX = -this.radius; rX <= this.radius; rX++)
+				for(int relY = -this.radius; relY <= this.radius; relY++)
+					for(int relX = -this.radius; relX <= this.radius; relX++)
 					{
-						int cX = x+rX;
-						int cY = y+rY;
+						int cX = x+relX;
+						int cY = y+relY;
 
-						if(cX < 0 || cY < 0 || cX >= input.getWidth() || cY >= input.getHeight())
+						if(cX < 0 || cY < 0 || cX >= input.getWidth() || cY >= input.getHeight()) // in bounds?
 							continue;
+
 						if(input.get(cX, cY))
 							sum++;
 					}
 
-				double proportion = sum/diameter*diameter;
+				double proportion = sum/(diameter*diameter);
 
 				// keep old true-pixels
 				if(input.get(x, y))
