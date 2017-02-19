60629d27abe4ed73ec1e113398206d5f06c7f739
Dominik <dominik.weissenseel@gmx.de>
2017-02-19 21:47:35 +0100

    Created first version of IslandHeightMapGenerator

--- src/main/java/de/domisum/exziff/island/heightmap/IslandHeightMapGenerator.java
+++ src/main/java/de/domisum/exziff/island/heightmap/IslandHeightMapGenerator.java
@@ -2,6 +2,8 @@ package de.domisum.exziff.island.heightmap;
 
 import de.domisum.exziff.map.HeightMap;
 import de.domisum.exziff.map.ShapeMap;
+import de.domisum.layeredopensimplexnoise.LayeredOpenSimplexNoise;
+import de.domisum.layeredopensimplexnoise.OctavedOpenSimplexNoise;
 
 public class IslandHeightMapGenerator
 {
@@ -25,7 +27,49 @@ public class IslandHeightMapGenerator
 	{
 		double[][] heights = new double[this.islandShape.getHeight()][this.islandShape.getWidth()];
 
-		return new HeightMap(heights);
+		LayeredOpenSimplexNoise noise = new OctavedOpenSimplexNoise(5, 0.1, 0.3, 1, 0.35, this.seed);
+		for(int y = 0; y < this.islandShape.getHeight(); y++)
+			for(int x = 0; x < this.islandShape.getWidth(); x++)
+			{
+				if(!this.islandShape.get(x, y))
+					continue;
+
+				heights[y][x] = noise.evaluate(x, y);
+			}
+
+		normalize(heights);
+
+		HeightMap islandHeightMap = new HeightMap(heights);
+		return islandHeightMap;
+	}
+
+	// TODO move this to own class
+	private static void normalize(double[][] heights)
+	{
+		double min = Double.MAX_VALUE;
+		double max = -Double.MAX_VALUE;
+
+		for(int y = 0; y < heights.length; y++)
+			for(int x = 0; x < heights[0].length; x++)
+			{
+				double height = heights[y][x];
+				if(height < min)
+					min = height;
+				if(height > max)
+					max = height;
+			}
+
+		double distance = max-min;
+
+		for(int y = 0; y < heights.length; y++)
+			for(int x = 0; x < heights[0].length; x++)
+			{
+				double height = heights[y][x];
+				height -= min;
+				height /= distance;
+
+				heights[y][x] = height;
+			}
 	}
 
 }
