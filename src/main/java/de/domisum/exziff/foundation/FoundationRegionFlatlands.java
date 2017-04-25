6f04641136553906d78f287e1a241446a851adcf
Dominik <dominik.weissenseel@gmx.de>
2017-04-25 21:07:17 +0200

    Implemented Flatlands

--- src/main/java/de/domisum/exziff/foundation/FoundationRegionFlatlands.java
+++ src/main/java/de/domisum/exziff/foundation/FoundationRegionFlatlands.java
@@ -1,7 +1,13 @@
 package de.domisum.exziff.foundation;
 
+import de.domisum.layeredopensimplexnoise.LayeredOpenSimplexNoise;
+import de.domisum.layeredopensimplexnoise.NoiseLayer;
+
 public class FoundationRegionFlatlands extends FoundationRegion
 {
+
+	// NOISE
+	private LayeredOpenSimplexNoise noise;
 
 	// INIT
 	public FoundationRegionFlatlands(int id, long seed)
@@ -13,12 +19,17 @@ public class FoundationRegionFlatlands extends FoundationRegion
 	// GENERATION
 	@Override public void generate()
 	{
+		NoiseLayer[] layers = new NoiseLayer[] {new NoiseLayer(50, 7, this.seed*397439834894893L*this.id),
+				new NoiseLayer(20, 2.5, this.seed*3848341111L*this.id)};
 
+		this.noise = new LayeredOpenSimplexNoise(layers);
 	}
 
 	@Override public int getHeightAt(int x, int z)
 	{
-		return 70;
+		double noiseValue = this.noise.evaluate(x, z);
+
+		return (int) Math.round(55+noiseValue);
 	}
 
 }
