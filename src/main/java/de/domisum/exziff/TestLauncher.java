821a9f243e6276d94a6bcc45482e8e0b5b658649
Dominik <dominik.weissenseel@gmx.de>
2017-06-30 13:15:49 +0200

    Completely cleared TestLauncher

--- src/main/java/de/domisum/exziff/TestLauncher.java
+++ src/main/java/de/domisum/exziff/TestLauncher.java
@@ -1,28 +1,11 @@
 package de.domisum.exziff;
 
-import de.domisum.exziff.world.World;
-import de.domisum.lib.auxilium.util.FileUtil;
-
-import java.io.File;
-
 public class TestLauncher
 {
 
 	public static void main(String[] args)
 	{
-		//islandTest();
-		//worldTest();
 
-		//worldGeneratorTest();
-	}
-
-	public static void worldGeneratorTest()
-	{
-		File worldFolder = new File("C:/Users/domisum/testChamber/testWorld");
-		FileUtil.deleteDirectory(worldFolder);
-
-		WorldGenerator worldGenerator = new WorldGenerator(1024*2, 8798989&1823305, worldFolder);
-		World world = worldGenerator.generate();
 	}
 
 }
