bdba06ff4adeae2d09b96cb2d143b0856a9e368a
Dominik <dominik.weissenseel@gmx.de>
2017-07-07 16:10:05 +0200

    Added TODO (PixelCoordinates)

--- src/main/java/de/domisum/exziff/PixelCoordinates.java
+++ src/main/java/de/domisum/exziff/PixelCoordinates.java
@@ -3,6 +3,7 @@ package de.domisum.exziff;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
+// TODO find replacement (more generalized) for this class
 @AllArgsConstructor
 @EqualsAndHashCode
 public class PixelCoordinates
