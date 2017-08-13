52a25154f1dc6c43a718acdd17aad5a385c2e352
Dominik <dominik.weissenseel@gmx.de>
2017-08-13 12:19:03 +0200

    Added random base polygon generation (ContinentsShapeGenerator)

--- src/main/java/de/domisum/exziff/shape/ContinentsShapeGenerator.java
+++ src/main/java/de/domisum/exziff/shape/ContinentsShapeGenerator.java
@@ -12,7 +12,9 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
+import java.util.Vector;
 
 public class ContinentsShapeGenerator extends BooleanMapGenerator
 {
@@ -24,6 +26,9 @@ public class ContinentsShapeGenerator extends BooleanMapGenerator
 	// settings
 	private int size;
 	private long seed;
+
+	@Getter @Setter private int desiredPolygonsBase = 3;
+	@Getter @Setter private int desiredPolygonsMaxDifference = 1;
 
 	@Getter @Setter private double desiredMaxSideLength = 0.05;
 	@Getter @Setter private double maxDivisionBasePointCenterOffset = 0.15;
@@ -65,12 +70,94 @@ public class ContinentsShapeGenerator extends BooleanMapGenerator
 		return this.output;
 	}
 
+
+	// BASE POLYGONS
 	private void generateBasePolygons()
 	{
-		this.polygons.add(new Polygon2D(new Vector2D(0.25, 0.25), new Vector2D(0.75, 0.25), new Vector2D(.75, .75)));
-		this.polygons.add(new Polygon2D(new Vector2D(0.1, 0.9), new Vector2D(.1, .4), new Vector2D(0.5, 0.9)));
+		int numberOfPolygonsToGenerate = RandomUtil
+				.distribute(this.desiredPolygonsBase, this.desiredPolygonsMaxDifference, this.random);
 
-		// TODO make sure the initially generated polygons are valid
+		// generate small base polygons
+		while(this.polygons.size() < numberOfPolygonsToGenerate)
+		{
+			Polygon2D newPolygon = generateMiniBasePolygon();
+
+			boolean valid = validateReplacementPolygon(newPolygon, null);
+			if(valid)
+				this.polygons.add(newPolygon);
+		}
+
+		// modify the base polygons
+		for(int i = 0; i < 1000; i++)
+		{
+			Polygon2D toModify = RandomUtil.getElement(this.polygons, this.random);
+
+			Polygon2D modified = modifyBasePolygon(toModify);
+
+
+			boolean valid = validateReplacementPolygon(modified, toModify);
+			if(valid)
+			{
+				this.polygons.remove(toModify);
+				this.polygons.add(modified);
+			}
+		}
+	}
+
+	private Polygon2D generateMiniBasePolygon()
+	{
+		double radius = 0.1;
+
+		double centerX = RandomUtil.getFromRange(this.minPolygonEdgeDistance, 1-this.minPolygonEdgeDistance, this.random);
+		double centerY = RandomUtil.getFromRange(this.minPolygonEdgeDistance, 1-this.minPolygonEdgeDistance, this.random);
+		Vector2D center = new Vector2D(centerX, centerY);
+
+		List<Vector2D> points = new Vector<>();
+		for(int i = 0; i < 3; i++)
+		{
+			Vector2D randomOffset = RandomUtil.getUnitVector2D(this.random).multiply(radius);
+
+			Vector2D point = center.add(randomOffset);
+			points.add(point);
+		}
+
+		return new Polygon2D(points);
+	}
+
+	private Polygon2D modifyBasePolygon(Polygon2D toModify)
+	{
+		if(RandomUtil.nextDouble(this.random) < 0.3)
+			return modifyBasePolygonMove(toModify);
+
+		return modifyBasePolygonDeform(toModify);
+	}
+
+	private Polygon2D modifyBasePolygonDeform(Polygon2D toModify)
+	{
+		Vector2D center = toModify.getPointCenter();
+		Vector2D point = RandomUtil.getElement(toModify.points, this.random);
+
+		Vector2D centerToPoint = point.subtract(center);
+		Vector2D ctpDirection = centerToPoint.normalize();
+		Vector2D ctpOrthogonal = center.orthogonal().normalize();
+
+		Vector2D pointDeltaForward = ctpDirection.multiply(RandomUtil.distribute(0.05, 0.1, this.random));
+		Vector2D pointDeltaSideward = ctpOrthogonal.multiply(RandomUtil.distribute(0.05, 0.1, this.random));
+		Vector2D pointDelta = pointDeltaForward.add(pointDeltaSideward);
+
+		Vector2D newPoint = point.add(pointDelta);
+		List<Vector2D> newPoints = new ArrayList<>(toModify.points);
+		newPoints.replaceAll((p)->Objects.equals(p, point) ? newPoint : p); // replace the old point with the new one
+
+		return new Polygon2D(newPoints);
+	}
+
+	private Polygon2D modifyBasePolygonMove(Polygon2D toModify)
+	{
+		Vector2D movementDirection = RandomUtil.getUnitVector2D(this.random);
+
+		Vector2D movement = movementDirection.multiply(RandomUtil.distribute(0.05, 0.1, this.random));
+		return toModify.move(movement);
 	}
 
 
