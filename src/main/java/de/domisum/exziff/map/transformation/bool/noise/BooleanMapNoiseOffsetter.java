c885a7aa62ff5b6898236e12b9eb1e509109c9df
Dominik <dominik.weissenseel@gmx.de>
2017-08-24 14:16:45 +0200

    Changed to use relative noise values only (BooleanMapNoiseOffsetter)

--- src/main/java/de/domisum/exziff/map/transformation/bool/noise/BooleanMapNoiseOffsetter.java
+++ src/main/java/de/domisum/exziff/map/transformation/bool/noise/BooleanMapNoiseOffsetter.java
@@ -4,6 +4,7 @@ import de.domisum.exziff.map.BooleanMap;
 import de.domisum.exziff.map.transformation.bool.BooleanMapTransformation;
 import de.domisum.layeredopensimplexnoise.LayeredOpenSimplexNoise;
 
+
 public class BooleanMapNoiseOffsetter extends BooleanMapTransformation
 {
 
@@ -12,9 +13,16 @@ public class BooleanMapNoiseOffsetter extends BooleanMapTransformation
 	private LayeredOpenSimplexNoise noiseY;
 
 
-	// -------
 	// INIT
-	// -------
+
+	/**
+	 * Offsets pixels by the ammount provided by the noise functions. The noise functions are given relative coordinates
+	 * and should give relative coordinates back. This means that if the noise functions return an offset of 0.1, the pixel
+	 * will be offset by 0.1 times the size of the map pixels.
+	 *
+	 * @param noiseX the noise function dictating x offset
+	 * @param noiseY the noise function dictating y offset
+	 */
 	public BooleanMapNoiseOffsetter(LayeredOpenSimplexNoise noiseX, LayeredOpenSimplexNoise noiseY)
 	{
 		this.noiseX = noiseX;
@@ -22,23 +30,20 @@ public class BooleanMapNoiseOffsetter extends BooleanMapTransformation
 	}
 
 
-	// -------
 	// TRANSFORMATION
-	// -------
 	@Override public BooleanMap transform(BooleanMap input)
 	{
-		boolean[][] pixels = new boolean[input.getHeight()][input.getWidth()];
+		double scale = input.getWidth();
 
+		boolean[][] pixels = new boolean[input.getHeight()][input.getWidth()];
 		for(int x = 0; x < input.getWidth(); x++)
 			for(int y = 0; y < input.getHeight(); y++)
 			{
-				if(!input.get(x, y))
+				if(!input.get(x, y)) // only offset true/white pixels
 					continue;
 
-				double scale = input.getWidth();
-
-				double dX = this.noiseX.evaluate(x/scale, y/scale);
-				double dY = this.noiseY.evaluate(x/scale, y/scale);
+				double dX = this.noiseX.evaluate(x/scale, y/scale)*scale;
+				double dY = this.noiseY.evaluate(x/scale, y/scale)*scale;
 
 				int nX = x+(int) Math.round(dX);
 				int nY = y+(int) Math.round(dY);
