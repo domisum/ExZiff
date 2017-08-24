34a252ac0af731ab4a0d329744679bd626f48586
Dominik <dominik.weissenseel@gmx.de>
2017-08-24 16:27:40 +0200

    First working version of ContinentShapeNoiseDeformer

--- src/main/java/de/domisum/exziff/shape/ContinentsShapeGenerator.java
+++ src/main/java/de/domisum/exziff/shape/ContinentsShapeGenerator.java
@@ -3,6 +3,7 @@ package de.domisum.exziff.shape;
 import de.domisum.exziff.map.BooleanMap;
 import de.domisum.exziff.map.generator.bool.BooleanMapGenerator;
 import de.domisum.exziff.map.generator.bool.BooleanMapPolygonGenerator;
+import de.domisum.exziff.map.transformation.bool.BooleanMapScale;
 import de.domisum.exziff.shape.continent.ContinentShapeBasePolygonGenerator;
 import de.domisum.exziff.shape.continent.ContinentShapeNoiseDeformer;
 import de.domisum.exziff.shape.continent.ContinentShapePolygonDeformer;
@@ -23,6 +24,7 @@ public class ContinentsShapeGenerator extends BooleanMapGenerator
 	// REFERENCES
 	private ContinentShapeBasePolygonGenerator basePolygonGenerator;
 	private ContinentShapePolygonDeformer polygonDeformer;
+	private BooleanMapScale scale = new BooleanMapScale(4);
 	private ContinentShapeNoiseDeformer noiseDeformer;
 
 
@@ -42,12 +44,16 @@ public class ContinentsShapeGenerator extends BooleanMapGenerator
 	// GENERATE
 	@Override public BooleanMap generate()
 	{
+		// generate using polygons
 		List<Polygon2D> polygons = this.basePolygonGenerator.generate();
 		polygons = this.polygonDeformer.deformPolygons(polygons);
 
 		BooleanMapPolygonGenerator generator = new BooleanMapPolygonGenerator(this.size/this.downscalingFactor, polygons);
 		BooleanMap continentShape = generator.generate();
 
+		continentShape = this.scale.transform(continentShape);
+
+		// deform
 		continentShape = this.noiseDeformer.deform(continentShape);
 
 		return continentShape;
--- src/main/java/de/domisum/exziff/shape/continent/ContinentShapeNoiseDeformer.java
+++ src/main/java/de/domisum/exziff/shape/continent/ContinentShapeNoiseDeformer.java
@@ -1,6 +1,13 @@
 package de.domisum.exziff.shape.continent;
 
 import de.domisum.exziff.map.BooleanMap;
+import de.domisum.exziff.map.transformation.bool.BooleanMapFloodFill;
+import de.domisum.exziff.map.transformation.bool.BooleanMapInvert;
+import de.domisum.exziff.map.transformation.bool.BooleanMapSmooth;
+import de.domisum.exziff.map.transformation.bool.noise.BooleanMapNoiseOffsetter;
+import de.domisum.layeredopensimplexnoise.LayeredOpenSimplexNoise;
+import de.domisum.layeredopensimplexnoise.NoiseLayer;
+import de.domisum.layeredopensimplexnoise.NoiseLayers;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +17,7 @@ public class ContinentShapeNoiseDeformer
 {
 
 	// SETTINGS
-	@Getter @Setter private int iterations;
+	@Getter @Setter private int iterations = 3;
 
 	// REFERENCES
 	private Random random;
@@ -33,21 +40,39 @@ public class ContinentShapeNoiseDeformer
 
 		for(int i = 0; i < this.iterations; i++)
 		{
-			scatter();
+			scatter(i);
 			cleanup();
 		}
 
 		return this.map;
 	}
 
-	private void scatter()
+	private void scatter(int iteration)
 	{
+		NoiseLayers noiseLayers = new NoiseLayers(new NoiseLayer(0.03, 0.01, -1));
 
+		LayeredOpenSimplexNoise noiseXOffset = new LayeredOpenSimplexNoise(noiseLayers.getRandomSeedsCopy(this.random));
+		LayeredOpenSimplexNoise noiseYOffset = new LayeredOpenSimplexNoise(noiseLayers.getRandomSeedsCopy(this.random));
+
+
+		BooleanMapNoiseOffsetter noiseOffsetter = new BooleanMapNoiseOffsetter(noiseXOffset, noiseYOffset);
+		this.map = noiseOffsetter.transform(this.map);
 	}
 
 	private void cleanup()
 	{
+		BooleanMap deformedShape = this.map;
 
+		BooleanMapSmooth smooth = new BooleanMapSmooth(2, 0.3, 0.35);
+		deformedShape = smooth.transform(deformedShape);
+
+		BooleanMapFloodFill floodFill = new BooleanMapFloodFill();
+		deformedShape = floodFill.transform(deformedShape);
+
+		BooleanMapInvert invert = new BooleanMapInvert();
+		deformedShape = invert.transform(deformedShape);
+
+		this.map = deformedShape;
 	}
 
 }
