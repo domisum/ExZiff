1badd1df1311db4cea0388e7f5cde0bd4abf94bf
Dominik <dominik.weissenseel@gmx.de>
2017-09-02 18:00:24 +0200

    Removed color debug comments

--- src/main/java/de/domisum/exziff/TestLauncher.java
+++ src/main/java/de/domisum/exziff/TestLauncher.java
@@ -18,7 +18,7 @@ public class TestLauncher
 	private static int[][] colorsRGB = new int[][] {{240, 163, 255}, {0, 117, 220}, {153, 63, 0}, {76, 0, 92}, {25, 25, 25},
 			{0, 80, 49}, {43, 206, 72}, {255, 204, 153}, {128, 128, 128}, {143, 124, 0}, {157, 204, 0}, {194, 0, 136},
 			{0, 51, 128}, {255, 164, 5}, {75, 117, 0}, {255, 0, 16}, {94, 241, 242}, {0, 153, 143}, {224, 255, 102},
-			{116, 10, 255}, {153, 0, 0},  {255, 255, 0}, {255, 80, 5}};
+			{116, 10, 255}, {153, 0, 0}, {255, 255, 0}, {255, 80, 5}};
 
 	private static int[] colorsInt = new int[colorsRGB.length];
 
@@ -51,8 +51,8 @@ public class TestLauncher
 		for(int y = 0; y < regions.getHeight(); y++)
 			for(int x = 0; x < regions.getWidth(); x++)
 			{
-				//				if(!continentShape.get(x, y))
-				//					continue;
+				if(!continentShape.get(x, y))
+					continue;
 
 				int size = 64*4;
 				int redX = x/size;
