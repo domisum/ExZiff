d0234e34b6b2ccc238abadbc8cd6196fb9ebe3a7
Dominik <dominik.weissenseel@gmx.de>
2017-11-23 20:47:25 +0100

    Moved test code from Prototype to TestLauncher

--- src/test/java/de/domisum/exziff/Prototype.java
+++ src/test/java/de/domisum/exziff/Prototype.java
@@ -1,18 +1,7 @@
 package de.domisum.exziff;
 
-import de.domisum.exziff.generator.bedrockpartition.BedrockRegionPartitionGenerator;
-import de.domisum.exziff.generator.bedrockpartition.UniformlyDistributedPointsGenerator;
-import de.domisum.exziff.map.BooleanMap;
-import de.domisum.exziff.map.ShortMap;
-import de.domisum.exziff.map.converter.ShortMapToImageConverter;
-import de.domisum.exziff.map.generator.bool.BooleanMapFromImageGenerator;
-import de.domisum.lib.auxilium.util.FileUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.Random;
 
 public class Prototype
 {
@@ -27,24 +16,7 @@ public class Prototype
 
 	private void run()
 	{
-		this.logger.info("Starting image loading");
-		BooleanMapFromImageGenerator fromImageGenerator = new BooleanMapFromImageGenerator(0.5);
-		BooleanMap continentShape = fromImageGenerator
-				.generate(FileUtil.readImage(new File("C:\\Users\\domisum\\testChamber\\exziff\\res\\continentShape.png")));
-		this.logger.info("Image loading done");
 
-
-		Random random = new Random(3881);
-		BedrockRegionPartitionGenerator bedrockRegionPartitionGenerator = new BedrockRegionPartitionGenerator(
-				new UniformlyDistributedPointsGenerator());
-		ShortMap regions = bedrockRegionPartitionGenerator.generate(random.nextLong(), continentShape);
-
-
-		this.logger.info("Starting export");
-		ShortMapToImageConverter shortMapToImageConverter = new ShortMapToImageConverter();
-		BufferedImage image = shortMapToImageConverter.convert(regions);
-		FileUtil.writeImage(new File("C:\\Users\\domisum\\testChamber\\exziff//regions.png"), image);
-		this.logger.info("Export done");
 	}
 
 }
--- src/test/java/de/domisum/exziff/TestLauncher.java
+++ src/test/java/de/domisum/exziff/TestLauncher.java
@@ -1,22 +1,50 @@
 package de.domisum.exziff;
 
-import de.domisum.exziff.generator.WorldGenerator;
+import de.domisum.exziff.generator.bedrockpartition.BedrockRegionPartitionGenerator;
+import de.domisum.exziff.generator.bedrockpartition.UniformlyDistributedPointsGenerator;
+import de.domisum.exziff.map.BooleanMap;
+import de.domisum.exziff.map.ShortMap;
+import de.domisum.exziff.map.converter.ShortMapToImageConverter;
+import de.domisum.exziff.map.generator.bool.BooleanMapFromImageGenerator;
+import de.domisum.lib.auxilium.util.FileUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.Random;
 
 public class TestLauncher
 {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+
 	public static void main(String[] args)
 	{
-		Prototype.start();
+		new TestLauncher().test();
+	}
 
-		if(System.currentTimeMillis() > 0)
-			return;
+	private void test()
+	{
+		logger.info("Starting image loading");
+		BooleanMapFromImageGenerator fromImageGenerator = new BooleanMapFromImageGenerator(0.5);
+		BooleanMap continentShape = fromImageGenerator
+				.generate(FileUtil.readImage(new File("C:\\Users\\domisum\\testChamber\\exziff\\res\\continentShape.png")));
+		logger.info("Image loading done");
 
-		WorldGenerator generator = new WorldGenerator((int) Math.pow(2, 14), 1338,
-				new File("C:\\Users\\domisum\\testChamber\\exziff\\testWorld"));
-		generator.generate();
+
+		Random random = new Random(3881);
+		BedrockRegionPartitionGenerator bedrockRegionPartitionGenerator = new BedrockRegionPartitionGenerator(
+				new UniformlyDistributedPointsGenerator());
+		ShortMap regions = bedrockRegionPartitionGenerator.generate(random.nextLong(), continentShape);
+
+
+		this.logger.info("Starting export");
+		ShortMapToImageConverter shortMapToImageConverter = new ShortMapToImageConverter();
+		BufferedImage image = shortMapToImageConverter.convert(regions);
+		FileUtil.writeImage(new File("C:\\Users\\domisum\\testChamber\\exziff//regions.png"), image);
+		this.logger.info("Export done");
 	}
 
 }
